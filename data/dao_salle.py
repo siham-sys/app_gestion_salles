086725b85f40219f14354f6c88091f8e6f2af5c9
Siham-sys <sihamighmouracene2002@gmail.com>
2026-04-19 12:06:10 -0400

    Methode get connection

---
+++ data/dao_salle.py
@@ -0,0 +1,15 @@
+import mysql.connector
+import json
+from models.salle import Salle
+
+class Datasalle :
+    def get_connection(self):
+        with open("./data/config.json","r") as f:
+            config = json.load(f)
+        conn= mysql.connector.connect(
+            host=config["host"],
+            user=config["user"],
+            password=config["password"],
+            database=config["database"]
+        )
+        return conn
