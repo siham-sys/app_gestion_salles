ec5c6142cb1b246d2506b929bc64d3b1b881a605
Siham-sys <sihamighmouracene2002@gmail.com>
2026-04-19 15:46:46 -0400

    Le fonctionnement de la classe ServiceSalle

--- main.py
+++ main.py
@@ -17,3 +17,22 @@ s1.capacite=50
 dao.update_salle(s1)
 
 dao.delete_salle("C250")
+
+from services.service_salle import  ServiceSalle
+from models.salle import Salle
+service = ServiceSalle()
+
+salle1=Salle("A207","Informatique","Lab",35)
+print(service.ajouter_salle(salle1))
+
+for d in service.recuperer_salles():
+    d.afficher_infos()
+
+d=service.rechercher_salle("A207")
+if d:
+    d.afficher_infos()
+
+salle1.capacite=50
+service.modifier_salle(salle1)
+
+service.supprimer_salle("A207")
