05bef2f366ea417ea007fb923fa4c749d1877a36
Siham-sys <sihamighmouracene2002@gmail.com>
2026-04-19 15:21:33 -0400

    Methode recherche_salle

--- services/service_salle.py
+++ services/service_salle.py
@@ -24,5 +24,8 @@ class ServiceSalle:
     def supprimer_salle(self, code):
         self.dao_salle.delete_salle(code)
 
+    def rechercher_salle(self, code):
+        return self.dao_salle.get_salle(code)
+
 
 
